baade357a0d2d20e17be0a4dca500b4ef63efd57
goliatone <burgosemi@gmail.com>
2017-06-05 16:40:01 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*jshint esversion:6, node:true*/
+'use strict';
+
+/*
+ * Default initializer for the module.
+ *
+ * You can always override this and make
+ * a custom initializer.
+ */
+module.exports.init = require('./lib/init');
